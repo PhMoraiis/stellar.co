c8eab5c6df7ae1890d0f1c69df3c324b7d463d02
Philipe Barbosa de Morais <liperapltda@gmail.com>
2023-04-08 13:44:07 -0300

    Update styles

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,13 +6,13 @@ const Hero = () => {
           <p className="text-[24px] font-coolveticaRg paragraph">
             Tecnologia e Inovação
           </p>
-          <h1 className="text-6xl text-white font-coolveticaBd mx-auto md:text-[6.5rem]">
+          <h1 className="text-6xl text-white font-coolveticaRg mx-auto md:text-[6.5rem]">
             Venha brilhar no nosso universo.
           </h1>
           <div className="items-center justify-center py-4 gap-x-3 space-y-6 sm:flex sm:space-y-0">
             <a
               href="javascript:void(0)"
-              className="block py-4 px-14 text-white text-[1.3rem] mt-2 font-coolveticaLt gradient rounded-[3rem]"
+              className="block py-4 px-14 text-white text-[1.3rem] mt-2 font-coolveticaRg gradient rounded-[3rem]"
             >
               Saiba Mais
             </a>
