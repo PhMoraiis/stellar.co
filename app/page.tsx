e2c4a7ac02d5dca57c98032f084d95fda709d695
PhMoraiis <liperapltda@gmail.com>
2023-08-13 11:39:18 -0300

    rename components

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,12 @@ import Section from './components/Section';
 import SectionClone from './components/SectionClone';
 import PreFooter from './components/PreFooter';
 import Footer from './components/Footer';
-import HomeTest from './components/HomeTest';
 
 export default function Home() {
   return (
     <div>
       <div className="bg-white">
-        <HomeTest />
+        <Hero />
       </div>
       <div className='bg-[#000100]'>
         <Section />
