8871f86f4b88158556cdc16b541d069894c0ca16
PhMoraiis <liperapltda@gmail.com>
2023-08-28 02:31:14 -0300

    update footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,7 +1,8 @@
 import { IoLogoInstagram, IoLogoTwitter } from "react-icons/io";
 import { BsFacebook } from "react-icons/bs";
-import Logo from "../Logo";
 import Link from "next/link";
+import LogoWhite from "../Logo";
+import AnimatedLink from "../AnimatedLink";
 
 const Footer = () => {
 
@@ -27,49 +28,44 @@ const Footer = () => {
 
   const renderFooterNavs = () => {
     return (
-      <ul className="flex flex-col gap-y-3">
-        {footerNavs.map((nav) => (
-          <li key={nav.label}>
-            <ul className="flex gap-x-4 py-2">
-              {nav.items.map((item) => (
-                <li key={item.name}>
-                  <a
-                    className="text-gray-700 text-sm hover:text-gray-500"
-                  >
-                    {item.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
+      <ul className="flex flex-col gap-y-3 text-gray-200">
+        <li>
+          <ul className="flex gap-x-4 py-2">
+            <li className="flex gap-x-4">
+              <AnimatedLink title={"Termos"} />
+              <AnimatedLink title={"Licenças"} />
+              <AnimatedLink title={"Privacidade"} />
+              <AnimatedLink title={"Sobre Nós"} />
+            </li>
+          </ul>
+        </li>
       </ul>
     );
   };
 
   return (
-    <footer className="pt-10 bg-[#f8f8ff]">
+    <footer className="pt-10 bg-[#171718]">
       <div className="max-w-screen-xl mx-auto px-4 md:px-8">
         <div className="justify-between items-center gap-12 md:flex">
           <div className="flex-1 max-w-lg">
             <Link href="/">
-              <Logo />
+              <LogoWhite />
             </Link>
           </div>
           <div className="flex flex-col mt-6 md:mt-0">
-            <h3 className="text-black text-[1rem] pb-4">Brilhe Conosco</h3>
-            <p className="text-gray-700 text-[0.9rem] pb-2">Venha fazer parte conosco da Stellar</p>
-            <Link href="https://www.instagram.com/stellar.ti/" className="inline-block py-2 px-2 text-black hover:bg-[#1aa8d7] font-coolveticaLt grayGradient rounded-md">
+            <h3 className="text-white text-[1rem] pb-4">Brilhe Conosco</h3>
+            <p className="text-gray-400 text-[0.9rem] pb-2">Venha fazer parte conosco da Stellar</p>
+            <Link href="https://www.instagram.com/stellar.ti/" className="inline-block py-2 px-2 text-white hover:bg-[#1aa8d7] font-coolveticaLt grayGradient rounded-md">
               Saiba Mais
             </Link>
           </div>
         </div>
         <div className="mt-10 py-10 border-t border-gray-300 items-center justify-between sm:flex">
           <div>{renderFooterNavs()}</div>
-          <p className="text-gray-600">
+          <p className="text-gray-300">
             © 2023 Stellar Inovações. Todos os direitos reservados.
           </p>
-          <div className="flex items-center gap-x-6 py-2 text-gray-700">
+          <div className="flex items-center gap-x-6 py-2 text-gray-300">
             <Link href="https://www.instagram.com/stellar.ti/">
               <BsFacebook className="w-8 h-8 hover:text-gray-500 duration-150" />
             </Link>
