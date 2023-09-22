742c4fdb381e5d671f47a2591ef80342970705b5
PhMoraiis <liperapltda@gmail.com>
2023-09-21 23:50:14 -0300

    adding timeout in the render hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Hero from './components/Hero';
 import Section from './components/Section';
 import SectionClone from './components/SectionClone';
@@ -11,20 +11,31 @@ import BelowText from './components/BelowText';
 import SectionMovie from './components/SectionMovie';
 
 export default function Home() {
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    // Simule a conclusão da animação após 6 segundos
+    setTimeout(() => {
+      setAnimationComplete(true);
+    }, 6000); // 6 segundos
+  }, []);
+
   return (
     <div>
       <div className="bg-[#f8f8ff]">
         <Hero />
       </div>
-      <div className='bg-[#010101]'>
-        <BelowText />
-        <Section />
-        <SectionMovie />
-        <SlideSection />
-        <SectionClone />
-        <PreFooter />
-        <Footer />
-      </div>
+      {animationComplete && (
+        <div className='bg-[#010101]'>
+          <BelowText />
+          <Section />
+          <SectionMovie />
+          <SlideSection />
+          <SectionClone />
+          <PreFooter />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
